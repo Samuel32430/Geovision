675b40b4b7d5032bd4b7238d2a2e44d74d5df431
Samuel32430 <samuel.vilca9120@gmail.com>
2026-06-10 18:40:00 -0500

    geovision

---
+++ src/main/java/com/example/geovision/GeovisionApplication.java
@@ -0,0 +1,13 @@
+package com.example.geovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeovisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeovisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geovision/GeovisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeovisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
